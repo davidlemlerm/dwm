5ba388b80367ad3938f1666ee448da7074c41c89
David Lemler <dlemler@pm.me>
2020-04-03 16:40:36 -0500

    Change color variable names

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx   = 1;        /* border pixel of windows */
+static const unsigned int snap       = 32;       /* snap pixel */
+static const int showbar             = 1;        /* 0 means no bar */
+static const int topbar              = 1;        /* 0 means bottom bar */
+static const char *fonts[]           = { "monospace:size=10" };
+static const char dmenufont[]        = "monospace:size=10";
+static const char col_dark_strong[]  = "#222222";
+static const char col_dark[]         = "#444444";
+static const char col_light[]        = "#bbbbbb";
+static const char col_light_strong[] = "#eeeeee";
+static const char col_accent[]       = "#005577";
+static const char *colors[][3]       = {
+	/*               fg                bg               border   */
+	[SchemeNorm] = { col_light,        col_dark_strong, col_dark },
+	[SchemeSel]  = { col_light_strong, col_accent,      col_accent  },
 };
 
 /* tagging */
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-c", "-l", "40", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_dark_strong, "-nf", col_light, "-sb", col_accent, "-sf", col_light_strong, "-c", "-l", "40", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
