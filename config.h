b69044002b2e6e5f1ca17f30144b9657d5b5bfec
David Lemler <dlemler@pm.me>
2020-04-15 23:08:39 -0500

    Applied fibonacci layout patch

--- config.h
+++ config.h
@@ -42,6 +42,10 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+        { "|M|",      centeredmaster },
+        { ">M>",      centeredfloatingmaster },
+        { "[@]",      spiral },
+        { "[\\]",      dwindle },
 };
 
 /* key definitions */
